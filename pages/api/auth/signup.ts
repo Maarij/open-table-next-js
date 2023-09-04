c066b95f67faf8ac2d8bd6a27fb87c25ff522bce
Maarij <maarij.anwar@gmail.com>
2023-09-04 08:17:53 -0500

    creating and sending JWT

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -2,6 +2,7 @@ import {NextApiRequest, NextApiResponse} from "next";
 import validator from "validator";
 import {PrismaClient} from "@prisma/client";
 import bcrypt from "bcrypt";
+import * as jose from "jose";
 
 const prisma = new PrismaClient();
 
@@ -81,10 +82,19 @@ export default async function handler(
         phone,
         email,
       }
-    })
+    });
+
+    const alg = "HS256";
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+    const token = await new jose
+      .SignJWT({email: user.email})
+      .setProtectedHeader({alg})
+      .setExpirationTime("24h")
+      .sign(secret);
 
     res.status(200).json({
-      hello: user
+      hello: token
     })
   }
 }
