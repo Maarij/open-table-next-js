450b8c9f0e7099bef183bcd93879a3d2267ffa19
Maarij <maarij.anwar@gmail.com>
2023-09-18 20:23:58 -0500

    fetching the bookings

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,5 +1,8 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {times} from "@/app/data";
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default async function handler(
   req: NextApiRequest,
@@ -28,5 +31,19 @@ export default async function handler(
     })
   }
 
-  return res.json({searchTimes});
+  const bookings = await prisma.booking.findMany({
+    where: {
+      booking_time: {
+        gte: new Date(`${day}T${searchTimes[0]}`),
+        lte: new Date(`${day}T${searchTimes[searchTimes.length-1]}`),
+      }
+    },
+    select: {
+      number_of_people: true,
+      booking_time: true,
+      tables: true
+    }
+  })
+
+  return res.json({searchTimes, bookings});
 }
