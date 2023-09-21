1ad9ec6417ba2fdf6ccc66af78fa7d5e14a0f7b1
Maarij <maarij.anwar@gmail.com>
2023-09-20 20:47:16 -0500

    validatin for reservation endpiont

--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -1,4 +1,7 @@
 import {NextApiRequest, NextApiResponse} from "next";
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default async function handler(
   req: NextApiRequest,
@@ -11,10 +14,22 @@ export default async function handler(
     partySize: string
   }
 
-  return res.json({
-    slug,
-    day,
-    time,
-    partySize
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug
+    }
   });
+
+  if (!restaurant) {
+    return res.status(400).json({
+      errorMessage: "Restaurant not found"
+    });
+  }
+
+  if ((new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`))
+    || (new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`))) {
+    return res.status(400).json({
+      errorMessage: "Restaurant is not open at that time"
+    });
+  }
 }
